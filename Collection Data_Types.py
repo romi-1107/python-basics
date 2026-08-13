030af8830e6297cdd87dc61d4cff9ddc362f7ab0
hadia-mubashir <hadiamubashir8048@gmail.com>
2026-08-13 01:16:55 -0700

    collections

--- Collection Data_Types.py
+++ Collection Data_Types.py
@@ -26,7 +26,7 @@ print('-Selling the Sheild.')
 print('- Upgrading Sword -> Magic GreatSword')
 
 backpack.remove('Sheild')
-inx = backpack.index('Sword')
+inx           = backpack.index('Sword')
 backpack[inx] = 'Magic GreatSword'
 print('🚨', backpack)
 print('-'*50)
@@ -34,7 +34,7 @@ print('-'*50)
 print('4. 🔎Checking backpack')
 print('🚨', backpack)
 
-total_count = len(backpack)
+total_count  = len(backpack)
 unique_items = set(backpack)
 unique_count = len(unique_items)
 potion_count = backpack.count('Potion')
@@ -42,3 +42,56 @@ potion_count = backpack.count('Potion')
 print(f'There are {total_count} Items in Total')
 print(f'There are {unique_count}Unique Items')
 print(f'There are {potion_count} Potions')
+print('-'*50)
+
+print('5.🙃Dropped the Backpack Upside-Down ... ')
+backpack.reverse()
+print('🚨', backpack)
+print('-'* 50)
+
+print('6. ➡Sorting Items')
+backpack.sort()
+
+print('🚨', backpack)
+print('-'* 50)
+
+print('7. 💤Sleeping...')
+
+a = backpack.pop()
+b = backpack.pop(2)
+c = backpack.pop()
+stolen = [a,b,c]
+
+print(f'Stolen: ',stolen)
+print('🚨', backpack)
+print('-'* 50)
+
+
+print('8. 💍Found Magic Ring And Coin Pouch')
+ring = 'Magic Ring'
+coin_pouch = ['Gold Coin','Silver Coin']
+
+
+backpack.insert(0,ring)
+backpack.append(coin_pouch)
+
+print('🚨', backpack)
+print('-'* 50)
+
+print('9. ✨Half Item Magically Disappeared. Damn You Magic Ring...')
+
+count = len(backpack)
+half = int(count / 2)
+backpack = backpack[:half]
+
+print('🚨',backpack)
+print('-'* 50)
+
+print('10. 👩‍🎓 Bandits Attack.')
+print('Backpack Stolen...')
+
+backpack = None
+print('🚨', backpack)
+print('-'* 50)
+
+
