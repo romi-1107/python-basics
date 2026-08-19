a6944af0929794c6b892876da815a51bed99f72e
hadia-mubashir <hadiamubashir8048@gmail.com>
2026-08-18 23:48:24 -0700

    collections

---
+++ Products.py
@@ -0,0 +1,122 @@
+Product = []
+Product_1:
+{
+
+    "id": "6a79b316f97b6f15bf6233c1",
+    "slug": "magnisure-n-pack",
+    "name": "Magnisure-n-Pack",
+    "shortDescription": "Supplement for daily use, boost energy and strength body, your wellness partner, no side effects",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets. It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.\n\n",
+    "detail": "<p class=\"ql-align-justify\"><strong style=\"background-color: rgb(255, 255, 255); color: rgb(0, 0, 0);\">Lorem&nbsp;Ipsum</strong><span style=\"background-color: rgb(255, 255, 255); color: rgb(0, 0, 0);\">&nbsp;is&nbsp;simply&nbsp;dummy&nbsp;text&nbsp;of&nbsp;the&nbsp;printing&nbsp;and&nbsp;typesetting&nbsp;industry.&nbsp;Lorem&nbsp;Ipsum&nbsp;has&nbsp;been&nbsp;the&nbsp;industry&#39;s&nbsp;standard&nbsp;dummy&nbsp;text&nbsp;ever&nbsp;since&nbsp;1966,&nbsp;when&nbsp;designers&nbsp;at&nbsp;Letraset&nbsp;and&nbsp;James&nbsp;Mosley,&nbsp;the&nbsp;librarian&nbsp;at&nbsp;St&nbsp;Bride&nbsp;Printing&nbsp;Library&nbsp;in&nbsp;London,&nbsp;took&nbsp;a&nbsp;1914&nbsp;Cicero&nbsp;translation&nbsp;and&nbsp;scrambled&nbsp;it&nbsp;to&nbsp;make&nbsp;dummy&nbsp;text&nbsp;for&nbsp;Letraset&#39;s&nbsp;Body&nbsp;Type&nbsp;sheets.&nbsp;It&nbsp;has&nbsp;survived&nbsp;not&nbsp;only&nbsp;many&nbsp;decades,&nbsp;but&nbsp;also&nbsp;the&nbsp;leap&nbsp;into&nbsp;electronic&nbsp;typesetting,&nbsp;remaining&nbsp;essentially&nbsp;unchanged.&nbsp;It&nbsp;was&nbsp;popularised&nbsp;thanks&nbsp;to&nbsp;these&nbsp;sheets&nbsp;and&nbsp;more&nbsp;recently&nbsp;with&nbsp;desktop&nbsp;publishing&nbsp;software&nbsp;like&nbsp;Aldus&nbsp;PageMaker&nbsp;and&nbsp;Microsoft&nbsp;Word&nbsp;including&nbsp;versions&nbsp;of&nbsp;Lorem&nbsp;Ipsum.</span></p>",
+    "price":{
+        "currency": "USD",
+        "amount": 3000,
+        "originalAmount": 3200
+},
+    "inventory":
+        {
+        "sku": "sku",
+        "stock": 20
+    },
+    "images": [
+        {
+            "id": "6a7c42dad7467e0ccd2c20d5",
+            "url": "https://grabmedin-development.s3.us-east-1.amazonaws.com/1786360574379-magnisure-n-2.webp",
+            "alt": "Magnisure-n-Pack",
+            "isPrimary": True,
+            "isDeleted": False,
+
+
+    {
+            "id": "6a7c42dad7467e0ccd2c20d6",
+            "url": "https://grabmedin-development.s3.us-east-1.amazonaws.com/1786360574381-magnisure-n.webp",
+            "alt": "Magnisure-n-Pack",
+            "isPrimary": False,
+            "isDeleted": False
+
+        }
+    ],
+    "categoryId": "69983fa30db00a5b51ca0285",
+    "status": "active",
+    "rating": {
+        "average": 4.4,
+        "count": 5
+    },
+    "createdAt": "2026-08-10T11:16:38.983Z",
+    "updatedAt": "2026-08-15T05:24:30.265Z",
+    "packs": [
+        {
+            "quantity": 3,
+            "price": 3000,
+            "compareAtPrice": 5000,
+            "sku": "any",
+            "stock": 20,
+            "isActive": True,
+            "_id": "6a7c42dad7467e0ccd2c20d3"
+        },
+        {
+            "quantity": 3,
+            "price": 6000,
+            "compareAtPrice": 9000,
+            "sku": "pack-3",
+            "stock": 55,
+            "isActive": True,
+            "_id": "6a7c42dad7467e0ccd2c20d4"
+        }
+    ],
+    "features": [
+        "Calcium updated",
+        "Magnisium",
+        "Vitamin D3",
+        "Vitamin B1",
+        "Vitamin B6",
+        "Vitamin B12",
+        "Folic Acid"
+    ],
+    "benefits": [
+        "Boosts energy updated",
+        "Good in pregnancy",
+        "Good in muscle cramps",
+        "Multi vitamins ",
+        "No side effects "
+    ],
+    "usage": [
+        "One spoon daily with a glass of water updated",
+        "or One spoon daily with a glass of milk",
+        "Not more than one spoon",
+        "Drink more water than usual",
+        "Good in pregnancy"
+    ],
+    "indications": [
+        "Dizziness updated",
+        "Cronic Fatigue",
+        "Muscle cramps",
+        "Low energy"
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
