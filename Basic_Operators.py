c194e130d3bf747a3ecf3cac3c407b245f3978c3
hadia-mubashir <hadiamubashir8048@gmail.com>
2026-08-19 00:59:52 -0700

    collections

---
+++ Basic_Operators.py
@@ -0,0 +1,51 @@
+x = 10
+y = 20
+z = x + y
+print(x, y, z)
+print(x + y + z)
+
+a = 'Hello'
+b = 'World'
+c = a + b
+print(c)
+print(a + b)
+name = 'Erik'
+print('My name is:' + name)
+print('My name is:{}'.format('Erik'))
+
+print('Hello')
+print('-' * 100)
+print('Hello' * 100)
+print('Hello\n' * 100)
+message = 'We need to build a brick wall'
+print('brick' in message)
+print('Brick' in message)
+print('Glass' in message)
+print('Glass' not in message)
+
+a = 'Concrete-10cm'
+b = 'Concrete-20cm'
+print(a == b)
+print(a != b)
+
+a = 10
+b = 3
+print("a+b=", a + b)
+print("a-b=", a - b)
+print("a*b=", a * b)
+print("a%b=", a % b)
+print("a/b=", a / b)
+print("a//b=", a // b)
+print("a**b=", a ** b)
+
+mats_1 = ["Concrete", "Steel", "Glass"]
+mats_2 = ["Wood", "Brick"]
+mats_3 = mats_1 + mats_2
+print(mats_3)
+print(mats_1 * 10)
+
+print('Concrete' in mats_1)
+print('Wool' in mats_1)
+print('Wool' not in mats_1)
+print(mats_1 == mats_2)
+print(mats_1 != mats_2)
